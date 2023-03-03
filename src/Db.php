9fbf69be835ac8cec9bd14dc6a0bdb41ff705c3d
Michael Garifullin <michael.garifullin@smartbox.com>
2023-03-03 23:57:29 +0000

    add seed db

--- src/Db.php
+++ src/Db.php
@@ -20,13 +20,8 @@ class Db
         $charset = 'utf8mb4';
 
         $dsn = "mysql:host=$host;dbname=$db;port=$port;charset=$charset";
-        $options = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
         try {
-            $this->con = new PDO($dsn, $user, $pass, $options);
+            $this->con = new PDO($dsn, $user, $pass);
         } catch (\PDOException $e) {
             throw new \PDOException($e->getMessage(), (int)$e->getCode());
         }
@@ -35,9 +30,9 @@ class Db
     /**
      * @return array|P[]
      */
-    public function get(): array
+    public final function get(): array
     {
-        $stmt = $this->con->query('SELECT * FROM p');
+        $stmt = $this->con->query('SELECT * FROM person');
         $result = [];
         while ($row = $stmt->fetch())
         {
@@ -45,5 +40,49 @@ class Db
         }
 
         return $result;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public final function seedDb(): string
+    {
+        $drop = $this->con->query('DROP TABLE IF EXISTS person');
+        if (!$drop->execute()) {
+            throw new \Exception('can not drop table');
+        }
+
+        $create = $this->con->query("
+            CREATE TABLE IF NOT EXISTS person (
+            `id` int DEFAULT NULL,
+            `name` varchar(255) DEFAULT NULL,
+            `address` varchar(255) DEFAULT NULL,
+            `city` varchar(255) DEFAULT NULL
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+        "
+        );
+        if (!$create->execute()) {
+            throw new \Exception('can not create table');
+        }
+
+        $data = [
+            [1,'Mike','Pushkina 3','Saratov'],
+            [2,'Vik','Pomortseva 12','Moscow'],
+        ];
+        $stmt = $this->con->prepare("INSERT INTO person (id, name, address, city) VALUES (?,?,?,?)");
+        try {
+            $this->con->beginTransaction();
+            foreach ($data as $row)
+            {
+                $stmt->execute($row);
+            }
+            $this->con->commit();
+        }catch (\Throwable $e){
+            $pdo->rollback();
+            throw new \Exception('can  not insert');
+        }
+
+
+        return 'Db have been seed';
     }
 }
